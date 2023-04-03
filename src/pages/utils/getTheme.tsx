a25a88d7e03d8de3e8f0dc60a43ad33d2fd6f78d
Pranshu Jain <86917304+Pranshu321@users.noreply.github.com>
2023-04-03 21:46:15 +0530

    Delete src/pages/utils directory

--- src/pages/utils/getTheme.tsx
+++
@@ -1,8 +0,0 @@
-import { ThemeType, themes, unknownTheme } from "../static/theme";
-
-export const getTheme = (theme: string): ThemeType => {
-  if (Object.prototype.hasOwnProperty.call(themes, theme)) {
-    return themes[theme as keyof typeof themes];
-  }
-  return unknownTheme;
-};
